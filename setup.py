b1bcbb9301ed1955fdedf28e19932ad22ac1356f
Prarthana Ganegama <105261574+GKPrarthana@users.noreply.github.com>
2025-04-19 15:56:13 +0530

    data ingestion added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate_speech_classification",
+    version="0.0.1",
+    author="GKPrarthana",
+    author_email="39-dba-0025@kdu.ac.lk",
+    packages=find_packages(),
+    install_requires=[],
+)
